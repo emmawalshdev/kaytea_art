e99fa7c233d41b154f42c9a40ae7287f0c92ad0d
emmahartedev <emmaharte33@outlook.com>
2021-08-07 13:53:17 +0000

    Add product views and template

--- products/models.py
+++ products/models.py
@@ -12,6 +12,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=200, null=True, blank=True)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def all_products(request):
+    '''A view to show all products, inc search and sorting queries'''
+    # return all products
+    products = Product.objects.all()
+
+    # Make products available in template
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)

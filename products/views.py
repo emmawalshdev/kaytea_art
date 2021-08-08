281c7db86cdaf8d7533d774db8e7421015877ebf
emmahartedev <emmaharte33@outlook.com>
2021-08-08 18:42:25 +0000

    Add sorting functionality

--- products/views.py
+++ products/views.py
@@ -8,15 +8,32 @@ def all_products(request):
     '''A view to show all products, inc search and sorting queries'''
     # return all products
     products = Product.objects.all()
+    all_categories = Category.objects.all()
     # ensure that an error is not thrown when no q is specified
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -26,11 +43,15 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     # Make products available in template
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'all_categories': all_categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)

6c3aa4d7a7e01019067eec2c5b8bee4e5b18a2de
emmahartedev <emmaharte33@outlook.com>
2021-08-15 20:00:13 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
+
 
 # Create your views here.
 def view_bag(request):
     '''A view to render the bag content page'''
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    '''Add a quantity of the specified product to the bag'''
+
+    product = Product.objects.get(pk=item_id)
+    # get quantity added
+    quantity = int(request.POST.get('quantity'))
+    # get redirect from form to redirect when process is complete
+    redirect_URL = request.POST.get('redirect_url')
+    # check if bag in session, create one if not
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        # update quantity if item already exists 
+        bag[item_id] == quantity
+    else:
+        # add item to bag
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    # overwrite the session var with updated version
+    request.session['bag'] = bag
+    return redirect(redirect_URL)
 
 def adjust_bag(request, item_id):
     '''Adjust the quantity of the specified product to the requested amount'''
@@ -33,23 +59,4 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         return HttpResponse(status=500)
 
-def add_to_bag(request, item_id):
-    '''Add a quantity of the specified product to the bag'''
 
-    # get quantity added
-    quantity = int(request.POST.get('quantity'))
-    # get redirect from form to redirect when process is complete
-    redirect_URL = request.POST.get('redirect_url')
-    # check if bag in session, create one if not
-    bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        # update quantity if item already exists 
-        bag[item_id] == quantity
-    else:
-        # add item to bag
-        bag[item_id] = quantity
-
-    # overwrite the session var with updated version
-    request.session['bag'] = bag
-    return redirect(redirect_URL)

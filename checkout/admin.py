ee6f55943ed095a8a1e7c3635abbec324b3f8166
emmahartedev <emmaharte33@outlook.com>
2021-08-17 12:34:10 +0000

    rename class fields in checkout model and remove tel number

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+    fields = ('order_number', 'date', 'first_name', 'last_name', 'email',
+              'address_line1', 'address_line2', 'town_or_city', 'county',
+              'country', 'postcode', 'delivery_cost', 'order_total', 
+              'grand_total',)

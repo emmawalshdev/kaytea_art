29671d1187fb31b234407e5d68e48efbddbfa319
emmahartedev <emmaharte33@outlook.com>
2021-08-29 14:39:49 +0000

    Add all blogs view and template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
+
 
 # Create your views here.
+def all_blogs(request):
+    '''A view to return all blogs'''
+    blogs = Blog.objects.all()
+
+    context = {
+        'blogs': blogs,
+    }
+
+    return render(request, 'blog/blog.html', context)

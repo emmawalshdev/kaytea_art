4b5d22c1cfd1bade2f2a542c9193faada30f4947
emmahartedev <emmaharte33@outlook.com>
2021-08-29 10:42:25 +0000

    Add blog app and models

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Blog, Keyword
+
+# Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(Keyword)
---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+# Create your models here.
+
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    slug = models.SlugField(max_length=254, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+    keyword = models.ManyToManyField(Keyword, blank=True)
+    title = models.CharField(max_length=254)
+    body = models.TextField(validators=[MinLengthValidator(20)])
+    image_url = models.URLField(max_length=1000, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
+

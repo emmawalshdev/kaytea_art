3556db5f3b2aa24c1b242550b946ad55039ece26
emmahartedev <emmaharte33@outlook.com>
2021-09-02 21:25:50 +0000

    Add 'edit blog reply' template and url

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('add/', views.add_blog, name='add_blog'),
     path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
     path('delete/<int:blog_id>/', views.delete_blog, name='delete_blog'),
+    path('edit_reply/<int:reply_id>/', views.edit_reply,
+         name='edit_reply'),
     path('delete_reply/<int:reply_id>/', views.delete_reply,
          name='delete_reply'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -129,10 +129,19 @@ def delete_blog(request, blog_id):
 @login_required
 def delete_reply(request, reply_id):
     """ Delete a comment from a blog post """
+    reply = get_object_or_404(Reply, pk=reply_id)
+    user = User.objects.get(username=request.user.username)
+    author = reply.author
 
-    if not request.user.is_superuser:
+    if reply.author != request.user:
+        print('not equal')
+    else:
+        print('equal')
+
+    if reply.author != user and not request.user.is_superuser:
         messages.error(request, 'Sorry, you do not have permssion \
             to delete a comment.')
+        print(user, author)
         return redirect(reverse('blog'))
 
     reply = get_object_or_404(Reply, pk=reply_id)
@@ -140,3 +149,38 @@ def delete_reply(request, reply_id):
     messages.success(request, 'Comment successfully deleted.')
 
     return redirect(reverse('blog'))
+
+
+@login_required
+def edit_reply(request, reply_id):
+    """ A view to edit a blog post """
+    reply = get_object_or_404(Reply, pk=reply_id)
+    blog_id = reply.blog.id
+    blog = get_object_or_404(Blog, pk=blog_id)
+    user = User.objects.get(username=request.user.username)
+    template = 'blog/edit_reply.html'
+    if reply.author != user and not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('blog'))
+
+    '''
+    if request.method == 'POST':
+        form = ReplyForm(request.POST, request.FILES, instance=reply)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated comment!')
+            return redirect(reverse('blog_detail', args=[reply.id]))
+        else:
+            messages.error(request, 'Failed to update comment. Please ensure the form is valid.')
+    else:
+        form = ReplyForm(instance=reply)
+        messages.info(request, f'You are editing your comment for {reply.blog}')
+
+    template = 'blog/edit_reply.html'
+        '''
+    context = {
+        'reply': reply,
+        'blog': blog,
+    }
+
+    return render(request, template, context)

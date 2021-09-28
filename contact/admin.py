9dc6358824b3b7c1abc3fd3be577b9bbf5f620ea
emmahartedev <emmaharte33@outlook.com>
2021-09-28 19:15:05 +0000

    Change admin display for contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Contact
 
-# Register your models here.
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'date',
+    )
+
+    list_filter = ('date',)
+
+
+admin.site.register(Contact, ContactAdmin)

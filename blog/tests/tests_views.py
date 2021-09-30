a87d8658ab4de7a387672afccd5329cfb7cae185
emmahartedev <emmaharte33@outlook.com>
2021-09-30 13:06:51 +0000

    Add view tests to blog app

---
+++ blog/tests/tests_views.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from blog.models import Keyword, Blog, Reply
+import json
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.blog_url = reverse('blog')
+        self.blog_detail_url = reverse('blog_detail', args=[1])
+        self.blog1 = Blog.objects.create(
+            id=1
+        )
+        self.add_blog_url = reverse('add_blog')
+
+    def test_all_blogs(self):
+
+        response = self.client.get(self.blog_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog.html')
+
+    def test_blog_detail_GET(self):
+
+        response = self.client.get(self.blog_detail_url)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/blog_detail.html')

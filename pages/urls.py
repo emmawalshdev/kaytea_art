014c298f2ab65c23ae8bc14cba79f6dcf629312c
emmahartedev <emmaharte33@outlook.com>
2022-02-20 18:03:38 +0100

    Add terms & conditions url & view

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import privacy_view
+from .views import privacy_view, terms_view
 
 urlpatterns = [
     path('privacy/', privacy_view, name="privacy"),
+    path('terms/', terms_view, name="terms"),
 ]
